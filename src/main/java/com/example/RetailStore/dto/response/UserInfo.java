8cb99b39a8389e9066fd37dd63ab6f278595316f
tunght2 <tunght2@ncb-bank.vn>
2025-11-13 22:02:40 +0700

    push

--- src/main/java/com/example/RetailStore/dto/response/UserInfo.java
+++ src/main/java/com/example/RetailStore/dto/response/UserInfo.java
@@ -1,12 +1,12 @@
 package com.example.RetailStore.dto.response;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserInfo {
     private String userId;
     private String username;
